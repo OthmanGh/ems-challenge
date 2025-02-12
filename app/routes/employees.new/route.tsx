981a0e355a641789c2d3e3e07b525ae9de0fb666
Osman <othmanghandour51@gmail.com>
2025-02-12 02:28:47 +0200

    feat: add employee form to new empmloyee page route

--- app/routes/employees.new/route.tsx
+++ app/routes/employees.new/route.tsx
@@ -1,4 +1,7 @@
-import { Form, redirect, type ActionFunction } from "react-router";
+import { redirect, type ActionFunction } from "react-router";
+import BackButton from "~/components/backButton";
+import EmployeeForm from "~/components/employeeForm";
+import LinkButton from "~/components/linkButton";
 import { getDB } from "~/db/getDB";
 
 export const action: ActionFunction = async ({ request }) => {
@@ -6,30 +9,16 @@ export const action: ActionFunction = async ({ request }) => {
   const full_name = formData.get("full_name");
 
   const db = await getDB();
-  await db.run(
-    'INSERT INTO employees (full_name) VALUES (?)',
-    [full_name]
-  );
+  await db.run("INSERT INTO employees (full_name) VALUES (?)", [full_name]);
 
   return redirect("/employees");
-}
+};
 
 export default function NewEmployeePage() {
   return (
-    <div>
-      <h1>Create New Employee</h1>
-      <Form method="post">
-        <div>
-          <label htmlFor="full_name">Full Name</label>
-          <input type="text" name="full_name" id="full_name" required />
-        </div>
-        <button type="submit">Create Employee</button>
-      </Form>
-      <hr />
-      <ul>
-        <li><a href="/employees">Employees</a></li>
-        <li><a href="/timesheets">Timesheets</a></li>
-      </ul>
+    <div className="flex flex-col gap-4 page_padding">
+      <BackButton label="Go Back" />
+      <EmployeeForm view="create" />
     </div>
   );
 }
